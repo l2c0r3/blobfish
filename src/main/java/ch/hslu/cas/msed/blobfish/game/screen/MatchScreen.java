afb1be139f7ae15b28eef84b10ad69ae508f95df
oirad <dario.huza@gmail.com>
2025-12-19 14:28:41 +0100

    fix #5 implement move input using the SAN annotation

--- src/main/java/ch/hslu/cas/msed/blobfish/game/screen/MatchScreen.java
+++ src/main/java/ch/hslu/cas/msed/blobfish/game/screen/MatchScreen.java
@@ -7,6 +7,9 @@ import ch.hslu.cas.msed.blobfish.game.OutputWriter;
 import ch.hslu.cas.msed.blobfish.game.exceptions.GameAbortedException;
 import ch.hslu.cas.msed.blobfish.game.exceptions.MatchAbortedException;
 import ch.hslu.cas.msed.blobfish.player.AbstractPlayer;
+import ch.hslu.cas.msed.blobfish.player.BotPlayer;
+import ch.hslu.cas.msed.blobfish.player.HumanPlayer;
+import ch.hslu.cas.msed.blobfish.player.exceptions.InvalidMoveException;
 
 public class MatchScreen {
     OutputWriter writer;
@@ -35,22 +38,42 @@ public class MatchScreen {
 
     public void start() throws GameAbortedException {
         while (!chessboard.isGameOver()) {
-            String move;
-            try {
-                move = currentPlayer.getNextMove(chessboard);
-            } catch (final MatchAbortedException e) {
-                writer.printlnAndFlush("Match aborted.");
-                return;
+            if (shouldDisplayBoard(currentPlayer)) {
+                printPosition(currentPlayer.getPlayerColor());
             }
 
-            // TODO: error handling
+            var move = getNextMove();
             chessboard.doMove(move);
-            printPosition(currentPlayer.getPlayerColor());
 
             currentPlayer = currentPlayer == white ? black : white;
         }
 
         writer.printlnAndFlush("Game over.");
+    }
+
+    private String getNextMove() throws GameAbortedException, MatchAbortedException {
+        String move = null;
+
+        do {
+            try {
+                move = currentPlayer.getNextMove(chessboard);
+            } catch (final InvalidMoveException e) {
+                if ("quit".equalsIgnoreCase(e.getOriginalMove())) {
+                    throw new MatchAbortedException("Match aborted by user.");
+                } else {
+                    writer.printlnAndFlush(e.getOriginalMove() + " is not a valid move.");
+                }
+            }
+        } while (move == null);
+        return move;
+    }
+
+    private Boolean shouldDisplayBoard(AbstractPlayer currentPlayer) {
+        if (white instanceof HumanPlayer && black instanceof HumanPlayer) {
+            return true;
+        } else if (white instanceof BotPlayer && black instanceof BotPlayer) {
+            return true;
+        } else return currentPlayer instanceof HumanPlayer;
     }
 
     private void printPosition(PlayerColor playerColor) {
--- src/main/java/ch/hslu/cas/msed/blobfish/player/HumanPlayer.java
+++ src/main/java/ch/hslu/cas/msed/blobfish/player/HumanPlayer.java
@@ -2,15 +2,26 @@ package ch.hslu.cas.msed.blobfish.player;
 
 import ch.hslu.cas.msed.blobfish.base.PlayerColor;
 import ch.hslu.cas.msed.blobfish.board.ChessBoard;
+import ch.hslu.cas.msed.blobfish.game.InputReader;
+import ch.hslu.cas.msed.blobfish.player.exceptions.InvalidMoveException;
 
 public class HumanPlayer extends AbstractPlayer {
 
-    public HumanPlayer(PlayerColor playerColor) {
+    InputReader reader;
+
+    public HumanPlayer(PlayerColor playerColor, InputReader reader) {
         super(playerColor);
+        this.reader = reader;
     }
 
     @Override
-    public String getNextMove(ChessBoard board) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public String getNextMove(ChessBoard board) throws InvalidMoveException {
+        var potentialMove = reader.readLine("Your move:");
+
+        if (board.isMoveLegal(potentialMove)) {
+            return potentialMove;
+        } else {
+            throw new InvalidMoveException(potentialMove, "The given move is not valid.");
+        }
     }
 }
