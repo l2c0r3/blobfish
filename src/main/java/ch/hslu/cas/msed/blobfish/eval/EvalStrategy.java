02760bc2a7caf73a1f4613c33afc32d7064e556f
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-01-03 16:08:14 +0100

    grammar

--- src/main/java/ch/hslu/cas/msed/blobfish/eval/EvalStrategy.java
+++ src/main/java/ch/hslu/cas/msed/blobfish/eval/EvalStrategy.java
@@ -4,7 +4,7 @@ package ch.hslu.cas.msed.blobfish.eval;
 public interface EvalStrategy {
 
     /**
-     * A positive number (e.g., +1.5) means White has an advantages;
+     * A positive number (e.g., +1.5) means White has an advantage;
      * a negative number (e.g., -2.0) means Black has the edge.
      */
     double getEvaluation(String positionFen);
