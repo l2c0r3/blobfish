299c56fdf78264d6b9e9ce57d421fc8df0f632be
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-01-04 11:37:47 +0100

    test

--- src/test/java/ch/hslu/cas/msed/blobfish/searchtree/MiniMaxTest.java
+++ src/test/java/ch/hslu/cas/msed/blobfish/searchtree/MiniMaxTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MiniMaxTest {
 
-    private static Stream<Arguments> oneMovePositionsProvider() {
+    private static Stream<Arguments> oneMovePositionsProviderWhite() {
         return Stream.of(
             Arguments.of(new ChessBoard("k7/8/8/3r4/8/3R4/8/K7 w - - 0 1"), "d3d5"),  // eat free rook
             Arguments.of(new ChessBoard("7k/8/5NPK/8/8/8/8/8 w - - 0 1"), "g6g7"),  // mateInOne
@@ -23,10 +23,10 @@ class MiniMaxTest {
     }
 
     @ParameterizedTest
-    @MethodSource("oneMovePositionsProvider")
-    void getBestNextMove_asWhite_materialStrategy_depth2_returnsExpected(ChessBoard board, String expectedNextMove) {
+    @MethodSource("oneMovePositionsProviderWhite")
+    void getBestNextMove_asWhite_materialStrategy_depth3_returnsExpected(ChessBoard board, String expectedNextMove) {
         // Arrange
-        var testee = new MiniMax(4, new MateAwareEval(new MaterialEval()), PlayerColor.WHITE);
+        var testee = new MiniMax(3, new MateAwareEval(new MaterialEval()), PlayerColor.WHITE);
 
         // Act
         String result = testee.getBestNextMove(board);
@@ -35,6 +35,25 @@ class MiniMaxTest {
         assertEquals(expectedNextMove, result);
     }
 
+    private static Stream<Arguments> oneMovePositionsProviderBlack() {
+        return Stream.of(
+                Arguments.of(new ChessBoard("7k/8/8/8/8/4p3/3R4/K7 b - - 0 1"), "e3d2"),  // eat free rook
+                Arguments.of(new ChessBoard("7k/8/8/8/8/pq6/8/K7 b - - 0 1"), "b3b2"),  // mateInOne
+                Arguments.of(new ChessBoard("8/8/5R2/8/3K4/8/2q5/k7 b - - 0 1"), "c2b2")  // dont now how its called
+        );
+    }
 
+    @ParameterizedTest
+    @MethodSource("oneMovePositionsProviderBlack")
+    void getBestNextMove_asBlack_materialStrategy_depth3_returnsExpected(ChessBoard board, String expectedNextMove) {
+        // Arrange
+        var testee = new MiniMax(3, new MateAwareEval(new MaterialEval()), PlayerColor.BLACK);
+
+        // Act
+        String result = testee.getBestNextMove(board);
+
+        // Assert
+        assertEquals(expectedNextMove, result);
+    }
 
 }
