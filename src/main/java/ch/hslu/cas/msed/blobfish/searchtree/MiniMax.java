e766b147679843869a99a634a88e32fdc5649023
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-01-04 09:56:08 +0100

    rename

--- src/main/java/ch/hslu/cas/msed/blobfish/searchtree/MiniMax.java
+++ src/main/java/ch/hslu/cas/msed/blobfish/searchtree/MiniMax.java
@@ -17,14 +17,14 @@ public class MiniMax {
         this.ownPlayerColor = ownPlayerColor;
     }
 
-    public String getBestNextMove(ChessBoard chessPosition) {
+    public String getBestNextMove(ChessBoard chessBoard) {
         Move bestMoveNextMove = null;
         var bestEval = PlayerColor.WHITE.equals(this.ownPlayerColor) ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
         var hasToMaximizingEvalBar = PlayerColor.WHITE.equals(this.ownPlayerColor);
         var nextPlayerColor = PlayerColor.WHITE.equals(this.ownPlayerColor) ? PlayerColor.BLACK : PlayerColor.WHITE;
 
-        for (var move : chessPosition.legalMoves()) {
-            var newPosition = chessPosition.doMove(getSanOfMove(move));
+        for (var move : chessBoard.legalMoves()) {
+            var newPosition = chessBoard.doMove(getSanOfMove(move));
             var eval = calcNextPosition(newPosition, calculationDepth - 1, nextPlayerColor);
 
             if (hasToMaximizingEvalBar) {
@@ -43,17 +43,17 @@ public class MiniMax {
         return bestMoveNextMove != null ? getSanOfMove(bestMoveNextMove) : null;
     }
 
-    private double calcNextPosition(ChessBoard position, int depth, PlayerColor playerAtTurn) {
-        if (depth <= 0 || position.isGameOver()) {
-            return evalStrategy.getEvaluation(position.getFen());
+    private double calcNextPosition(ChessBoard chessBoard, int depth, PlayerColor playerAtTurn) {
+        if (depth <= 0 || chessBoard.isGameOver()) {
+            return evalStrategy.getEvaluation(chessBoard.getFen());
         }
 
         var bestEval = PlayerColor.WHITE.equals(playerAtTurn) ? Double.NEGATIVE_INFINITY : Double.POSITIVE_INFINITY;
         var hasToMaximizingEvalBar = PlayerColor.WHITE.equals(playerAtTurn);
         var nextPlayerColor = PlayerColor.WHITE.equals(playerAtTurn) ? PlayerColor.BLACK : PlayerColor.WHITE;
 
-        for (var move : position.legalMoves()) {
-            var newPosition = position.doMove(getSanOfMove(move));
+        for (var move : chessBoard.legalMoves()) {
+            var newPosition = chessBoard.doMove(getSanOfMove(move));
             var eval = calcNextPosition(newPosition, depth - 1, nextPlayerColor);
 
             if (hasToMaximizingEvalBar) {
