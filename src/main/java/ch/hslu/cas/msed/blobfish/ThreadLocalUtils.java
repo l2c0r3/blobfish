e986a55e1bcfa46e7ac0b35ad8cbe30f453d8dcc
oirad <dario.huza@gmail.com>
2026-01-11 19:49:10 +0100

    improve ThreadLocalUtils

--- src/main/java/ch/hslu/cas/msed/blobfish/ThreadLocalUtils.java
+++ src/main/java/ch/hslu/cas/msed/blobfish/ThreadLocalUtils.java
@@ -1,8 +1,14 @@
 package ch.hslu.cas.msed.blobfish;
 
-import java.lang.reflect.Field;
+import lombok.NonNull;
 
-public class ThreadLocalUtils {
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+public final class ThreadLocalUtils {
+
+    private ThreadLocalUtils() {
+    }
 
     /**
      * Resets a static ThreadLocal field in the given class.
@@ -11,11 +17,16 @@ public class ThreadLocalUtils {
      * @param fieldName The name of the ThreadLocal field.
      * @throws ReflectiveOperationException If reflection fails.
      */
-    public static void resetStaticThreadLocal(Class<?> clazz, String fieldName)
+    public static void resetStaticThreadLocal(@NonNull Class<?> clazz, @NonNull String fieldName)
             throws ReflectiveOperationException {
 
         // Get the declared field (private/protected/public)
         Field field = clazz.getDeclaredField(fieldName);
+
+        if (!Modifier.isStatic(field.getModifiers())) {
+            throw new IllegalArgumentException("Field " + fieldName + " is not static");
+        }
+
         field.setAccessible(true);
 
         // Ensure it's a ThreadLocal
