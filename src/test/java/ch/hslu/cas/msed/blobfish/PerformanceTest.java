392d6391924deb222a9fa8944c1457566d9ff8a2
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-02-18 20:10:50 +0100

    Revert "refactore tet"
    This reverts commit 59f5c79477c759919a785c61993aaca77ae36784.

--- src/test/java/ch/hslu/cas/msed/blobfish/PerformanceTest.java
+++ src/test/java/ch/hslu/cas/msed/blobfish/PerformanceTest.java
@@ -1,10 +1,16 @@
 package ch.hslu.cas.msed.blobfish;
 
+import ch.hslu.cas.msed.blobfish.base.PlayerColor;
 import ch.hslu.cas.msed.blobfish.board.ChessBoard;
-import ch.hslu.cas.msed.blobfish.util.BotAlgorithmProvider;
+import ch.hslu.cas.msed.blobfish.eval.EvalStrategy;
+import ch.hslu.cas.msed.blobfish.eval.MateAwareEval;
+import ch.hslu.cas.msed.blobfish.eval.MaterialEval;
+import ch.hslu.cas.msed.blobfish.player.bot.minimax.MiniMaxAlgo;
 import ch.hslu.cas.msed.blobfish.util.FileUtil;
 import ch.hslu.cas.msed.blobfish.util.MeasurementUtil;
 import ch.hslu.cas.msed.blobfish.util.PlantUmlUtil;
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ScanResult;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.text.WordUtils;
@@ -12,11 +18,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.time.Duration;
@@ -25,19 +34,49 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Tag(value = "performance")
-public class PerformanceTest extends AbstractPositionTest {
+public class PerformanceTest {
 
     private static File rootFolderForMeasurements = null;
+
+    @FunctionalInterface
+    private interface MiniMaxAlgoConstructor {
+        MiniMaxAlgo create(int depth, EvalStrategy strategy, PlayerColor playerToMove);
+    }
+
+    private record PossibleStrategy(EvalStrategy strategy, String description) {
+    }
+
+    private record PositionToTest(String fen, PlayerColor playerToMove, String description) {
+    }
 
     private record MeasurementOfDepth(MeasurementUtil.MeasurementResult<String> measurementResult, int depth) {
     }
 
-    private record AlgorithmStrategy(String algorithm, BotAlgorithmProvider.PossibleStrategy strategy) {
+    private record AlgorithmStrategy(String algorithm, PossibleStrategy strategy) {
     }
 
-    private record StrategyDepth(BotAlgorithmProvider.PossibleStrategy strategy, int depth) {
+    private record StrategyDepth(PossibleStrategy strategy, int depth) {
+    }
+
+    private static final List<PossibleStrategy> possibleStrategies = List.of(
+            new PossibleStrategy(new MaterialEval(), "Simple material evaluation"),
+            new PossibleStrategy(new MateAwareEval(new MaterialEval()), "Mate aware material evaluation")
+    );
+
+    private static Stream<Arguments> positionProvider() {
+        return Stream.of(
+                Arguments.of(new PositionToTest("1r4r1/5p1k/p2p1q1p/2b1nPQ1/p7/6RP/B1R2PPK/2B5 b - - 0 1", PlayerColor.BLACK, "Complex position with many options")),
+                Arguments.of(new PositionToTest("5rk1/5q1p/p2PR1p1/4p1b1/1pP5/1P1Q4/P5PP/1K2R3 b - - 0 31", PlayerColor.BLACK, "Mid game - deflection - short")),
+                Arguments.of(new PositionToTest("r3k2r/pNp1ppbp/2nq2pn/7P/2B3b1/3P1N2/PPP2PP1/R1BQK2R b KQkq - 0 11", PlayerColor.BLACK, "Mid game - fork - short")),
+                Arguments.of(new PositionToTest("rn1q1r1k/pbpp1p1p/1p2p3/4P3/3P2Qp/P2B4/1PP2PPP/R4RK1 w - - 0 17", PlayerColor.WHITE, "Mid game - en passant - long")),
+                Arguments.of(new PositionToTest("r5k1/7p/1p1Qp1p1/p1np1r2/1q3P1P/1P6/2P3P1/RB3RK1 w - - 3 26", PlayerColor.WHITE, "Mid game - discovery - short")),
+                Arguments.of(new PositionToTest("8/5ppk/4p1p1/3pq3/3Q4/1B2r2P/P5P1/3R3K b - - 8 42", PlayerColor.BLACK, "End game - deflection - short")),
+                Arguments.of(new PositionToTest("5r1k/1pqnbr1P/p2p1pQp/2p5/3PP2P/1PN5/1PP3R1/R5K1 w - - 0 24", PlayerColor.WHITE, "Mid game - promotion - mate in 2 - short")),
+                Arguments.of(new PositionToTest("Q7/p1pk3p/2p2qp1/3p1b2/8/1PN1P3/P1PP2PP/R4KNR b - - 4 15", PlayerColor.BLACK, "Mid game - discovery - mate in 2 - short"))
+        );
     }
 
     @BeforeAll
@@ -49,21 +88,21 @@ public class PerformanceTest extends AbstractPositionTest {
 
     @ParameterizedTest
     @MethodSource(value = "positionProvider")
-    void measure_fens(PositionToTest positionToTest) {
-        var maxDepth = 2;
-        var numberOfMeasurements = 1;
+    void measure_startPos(PositionToTest positionToTest) {
+        var maxDepth = 4;
+        var numberOfMeasurements = 10;
         var chessboard = new ChessBoard(positionToTest.fen());
         var folderToSaveMeasurements = getFolderOfPosition(positionToTest, rootFolderForMeasurements);
         folderToSaveMeasurements.mkdirs();
 
         Map<AlgorithmStrategy, List<MeasurementOfDepth>> results = new HashMap<>();
 
-        BotAlgorithmProvider.getAllMiniMaxConstructors().forEach(miniMaxAlgoConstructor ->
-                BotAlgorithmProvider.possibleStrategies.forEach(strategy ->
+        getAllMiniMaxConstructors().forEach(miniMaxAlgoConstructor ->
+                possibleStrategies.forEach(strategy ->
                         IntStream.range(1, maxDepth + 1).forEach(depth -> {
 
 
-                            var miniMaxAlgoToTest = miniMaxAlgoConstructor.create(depth, strategy.strategy(), positionToTest.playerToMove());
+                            var miniMaxAlgoToTest = miniMaxAlgoConstructor.create(depth, strategy.strategy(), positionToTest.playerToMove);
                             var key = new AlgorithmStrategy(miniMaxAlgoToTest.getClass().getSimpleName(), strategy);
 
                             // do multiple measurements and calculate the median
@@ -280,6 +319,43 @@ public class PerformanceTest extends AbstractPositionTest {
 
     private static String getAlgorithmName(AlgorithmStrategy algorithmStrategy) {
         return String.format("%s (%s)", algorithmStrategy.algorithm(), algorithmStrategy.strategy().description());
+    }
+
+    private static List<MiniMaxAlgoConstructor> getAllMiniMaxConstructors() {
+        Class<?> base = MiniMaxAlgo.class;
+
+        try (ScanResult scan = new ClassGraph()
+                .enableClassInfo()
+                .acceptPackages("ch.hslu.cas.msed.blobfish")
+                .scan()) {
+
+            return scan.getSubclasses(base.getName()).loadClasses()
+                    .stream().map(clazz -> {
+                        try {
+                            var constructor = clazz.getDeclaredConstructor(int.class, EvalStrategy.class, PlayerColor.class);
+                            return (MiniMaxAlgoConstructor) (
+                                    int depth,
+                                    EvalStrategy strategy,
+                                    PlayerColor playerToMove
+                            ) -> instantiateAlgorithm(constructor, depth, strategy, playerToMove);
+                        } catch (NoSuchMethodException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).toList();
+        }
+    }
+
+    private static MiniMaxAlgo instantiateAlgorithm(
+            Constructor<?> constructor,
+            int depth,
+            EvalStrategy strategy,
+            PlayerColor playerToMove
+    ) {
+        try {
+            return (MiniMaxAlgo) constructor.newInstance(depth, strategy, playerToMove);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private Double mapMeasurementDepthToValue(MeasurementOfDepth measurementOfDepth) {
---
+++ src/test/java/ch/hslu/cas/msed/blobfish/SomeTest.java
@@ -0,0 +1,22 @@
+package ch.hslu.cas.msed.blobfish;
+
+import ch.hslu.cas.msed.blobfish.stockfish.StockFishService;
+import org.junit.jupiter.api.Test;
+
+class SomeTest {
+
+    @Test
+    void blub() throws Exception {
+        StockFishService stockFishService = new StockFishService.StockFishServiceBuilder()
+                .withMultiPV(3)
+                .withDefaultCalculationDepth(5)
+                .build();
+
+
+        stockFishService.setPosition("1r4r1/5p1k/p2p1q1p/2b1nPQ1/p7/6RP/B1R2PPK/2B5 b - - 0 1");
+        stockFishService.go();
+
+        stockFishService.close();
+    }
+
+}
