a554dee8374df4fe8f4609200e6899558ca5101a
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-03-06 17:00:16 +0100

    add test

--- src/test/java/ch/hslu/cas/msed/blobfish/stockfish/UciResponseParserTest.java
+++ src/test/java/ch/hslu/cas/msed/blobfish/stockfish/UciResponseParserTest.java
@@ -73,4 +73,44 @@ class UciResponseParserTest {
         assertEquals("e5f3", response.get(2));
     }
 
+
+    // position fen 1k6/ppp5/8/8/8/8/PPPP4/1K2R3 w - - 0 1
+    @Test
+    void parseBestMoves_mate_best_3_moves() {
+        // Arrange
+        String[] uciResponse = {
+                "info string Available processors: 0-23",
+                "info string Using 1 thread",
+                "info string NNUE evaluation using nn-c288c895ea92.nnue (125MiB, (102384, 1024, 15, 32, 1))",
+                "info string NNUE evaluation using nn-37f18f62d772.nnue (6MiB, (22528, 128, 15, 32, 1))",
+                "info string Network replica 1: Local memory. Shared memory not supported by the OS. Local allocation fallback.",
+                "info depth 1 seldepth 2 multipv 1 score mate 1 nodes 79 nps 79000 hashfull 0 tbhits 0 time 1 pv e1e8",
+                "info depth 1 seldepth 3 multipv 2 score cp 727 nodes 79 nps 79000 hashfull 0 tbhits 0 time 1 pv b2b3 c7c5",
+                "info depth 1 seldepth 3 multipv 3 score cp 694 nodes 79 nps 79000 hashfull 0 tbhits 0 time 1 pv a2a3 c7c5",
+                "info depth 2 seldepth 2 multipv 1 score mate 1 nodes 211 nps 211000 hashfull 0 tbhits 0 time 1 pv e1e8",
+                "info depth 2 seldepth 4 multipv 2 score cp 757 nodes 211 nps 211000 hashfull 0 tbhits 0 time 1 pv b2b4 c7c5",
+                "info depth 2 seldepth 3 multipv 3 score cp 745 nodes 211 nps 211000 hashfull 0 tbhits 0 time 1 pv c2c3 c7c5",
+                "info depth 3 seldepth 2 multipv 1 score mate 1 nodes 298 nps 298000 hashfull 0 tbhits 0 time 1 pv e1e8",
+                "info depth 3 seldepth 4 multipv 2 score cp 745 nodes 298 nps 298000 hashfull 0 tbhits 0 time 1 pv c2c3",
+                "info depth 3 seldepth 4 multipv 3 score cp 731 nodes 298 nps 298000 hashfull 0 tbhits 0 time 1 pv b2b4 c7c5 b4c5",
+                "info depth 4 seldepth 2 multipv 1 score mate 1 nodes 467 nps 467000 hashfull 0 tbhits 0 time 1 pv e1e8",
+                "info depth 4 seldepth 10 multipv 2 score cp 774 nodes 467 nps 467000 hashfull 0 tbhits 0 time 1 pv b2b4 b7b6",
+                "info depth 4 seldepth 5 multipv 3 score cp 741 nodes 467 nps 467000 hashfull 0 tbhits 0 time 1 pv c2c3 c7c5 c3c4 b8a8",
+                "info depth 5 seldepth 2 multipv 1 score mate 1 nodes 718 nps 718000 hashfull 0 tbhits 0 time 1 pv e1e8",
+                "info depth 5 seldepth 6 multipv 2 score cp 751 nodes 718 nps 718000 hashfull 0 tbhits 0 time 1 pv b2b4 a7a5 b4a5",
+                "info depth 5 seldepth 6 multipv 3 score cp 741 nodes 718 nps 718000 hashfull 0 tbhits 0 time 1 pv c2c3",
+                "bestmove e1e8"
+        };
+
+        // Act
+        var response = testee.parseBestMoves(uciResponse);
+
+        // Assert
+        assertFalse(response.isEmpty());
+        assertEquals(3, response.size());
+        assertEquals("e1e8", response.getFirst());
+        assertEquals("b2b4", response.get(1));
+        assertEquals("c2c3", response.get(2));
+    }
+
 }
