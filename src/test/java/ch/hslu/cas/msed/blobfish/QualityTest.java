bf8e7bee276769b8586b45e239239b45c55a65e2
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-03-08 21:54:20 +0100

    enable all

--- src/test/java/ch/hslu/cas/msed/blobfish/QualityTest.java
+++ src/test/java/ch/hslu/cas/msed/blobfish/QualityTest.java
@@ -66,46 +66,43 @@ class QualityTest {
                     "4k3/P7/8/8/8/8/7p/4K3 w - - 0 1",
                     // Precise pawn race calculation.
                     "8/3k4/8/2P5/8/8/4K2p/8 w - - 0 1"
+            )),
+            Map.entry(QualityTestCategory.DYNAMIC_PLAY, List.of(
+                    // Dynamic compensation test.
+                    "r3r1k1/pp1n1ppp/2pbpn2/q1p5/3P4/2N1PN2/PPQ1BPPP/2RR2K1 w - - 0 1",
+                    // Attack vs material choice.
+                    "r1b2rk1/pp1n1ppp/2p1pn2/q2p4/3P4/2NBPN2/PPQ2PPP/R1B2RK1 w - - 0 1",
+                    // Finds best defense.
+                    "r4rk1/ppp2ppp/2np1q2/4p3/2B1P3/2NP1Q2/PPP2PPP/R4RK1 b - - 0 1",
+                    // Quiet move over tactics.
+                    "r1bq1rk1/ppp2ppp/2np1n2/4p3/2B1P3/2NP1N2/PPP2PPP/R1BQ1RK1 b - - 0 1",
+                    // Best long-term plan.
+                    "r2q1rk1/pp2bppp/2npbn2/2p1p3/2P1P3/2NP1N2/PP2BPPP/R1BQ1RK1 w - - 0 1"
+            )),
+            Map.entry(QualityTestCategory.POSITIONAL_PLAY, List.of(
+                    // Positional evaluation test.
+                    "2r3k1/5pp1/p3p2p/1p1pP3/3P1P2/1P3N1P/P5P1/2R3K1 w - - 0 1",
+                    // Prophylaxis and restraint.
+                    "2r2rk1/1bq1bppp/p2ppn2/1pn5/4P3/1NN1BP2/PPQ2PBP/2RR2K1 w - - 0 1",
+                    // Best long-term plan.
+                    "r2q1rk1/pp2bppp/2npbn2/2p1p3/2P1P3/2NP1N2/PP2BPPP/R1BQ1RK1 w - - 0 1"
+            )),
+            Map.entry(QualityTestCategory.ENDGAMES_TECHNIQUE, List.of(
+                    // Basic opposition endgame.
+                    "8/8/8/3k4/8/4K3/4P3/8 w - - 0 1",
+                    // Distant passed pawn race.
+                    "8/2k5/8/2P5/8/5K2/6p1/8 w - - 0 1",
+                    // Rook activity and technique.
+                    "8/5pk1/6p1/8/3R4/6P1/5P1P/6K1 w - - 0 1",
+                    // Fortress recognition.
+                    "8/8/2p5/2Pp4/3P4/3K4/8/3k4 w - - 0 1",
+                    // Zugzwang and opposition.
+                    "8/8/8/2k5/2p5/2P5/3K4/8 w - - 0 1",
+                    // Queen endgame precision.
+                    "8/8/2k5/8/8/2K5/5Q2/6q1 w - - 0 1",
+                    // Bishop endgame evaluation.
+                    "8/5k2/6p1/5p2/3B1P2/6P1/5K2/8 w - - 0 1"
             ))
-
-//            Map.entry(QualityTestCategory.DYNAMIC_PLAY, List.of(
-//                    // Dynamic compensation test.
-//                    "r3r1k1/pp1n1ppp/2pbpn2/q1p5/3P4/2N1PN2/PPQ1BPPP/2RR2K1 w - - 0 1",
-//                    // Attack vs material choice.
-//                    "r1b2rk1/pp1n1ppp/2p1pn2/q2p4/3P4/2NBPN2/PPQ2PPP/R1B2RK1 w - - 0 1",
-//                    // Finds best defense.
-//                    "r4rk1/ppp2ppp/2np1q2/4p3/2B1P3/2NP1Q2/PPP2PPP/R4RK1 b - - 0 1",
-//                    // Quiet move over tactics.
-//                    "r1bq1rk1/ppp2ppp/2np1n2/4p3/2B1P3/2NP1N2/PPP2PPP/R1BQ1RK1 b - - 0 1",
-//                    // Best long-term plan.
-//                    "r2q1rk1/pp2bppp/2npbn2/2p1p3/2P1P3/2NP1N2/PP2BPPP/R1BQ1RK1 w - - 0 1"
-//            ))
-//
-//            Map.entry(TestCategory.POSITIONAL_PLAY, List.of(
-//                    // Positional evaluation test.
-//                    "2r3k1/5pp1/p3p2p/1p1pP3/3P1P2/1P3N1P/P5P1/2R3K1 w - - 0 1",
-//                    // Prophylaxis and restraint.
-//                    "2r2rk1/1bq1bppp/p2ppn2/1pn5/4P3/1NN1BP2/PPQ2PBP/2RR2K1 w - - 0 1",
-//                    // Best long-term plan.
-//                    "r2q1rk1/pp2bppp/2npbn2/2p1p3/2P1P3/2NP1N2/PP2BPPP/R1BQ1RK1 w - - 0 1"
-//            )),
-//
-//            Map.entry(TestCategory.ENDGAMES_TECHNIQUE, List.of(
-//                    // Basic opposition endgame.
-//                    "8/8/8/3k4/8/4K3/4P3/8 w - - 0 1",
-//                    // Distant passed pawn race.
-//                    "8/2k5/8/2P5/8/5K2/6p1/8 w - - 0 1",
-//                    // Rook activity and technique.
-//                    "8/5pk1/6p1/8/3R4/6P1/5P1P/6K1 w - - 0 1",
-//                    // Fortress recognition.
-//                    "8/8/2p5/2Pp4/3P4/3K4/8/3k4 w - - 0 1",
-//                    // Zugzwang and opposition.
-//                    "8/8/8/2k5/2p5/2P5/3K4/8 w - - 0 1",
-//                    // Queen endgame precision.
-//                    "8/8/2k5/8/8/2K5/5Q2/6q1 w - - 0 1",
-//                    // Bishop endgame evaluation.
-//                    "8/5k2/6p1/5p2/3B1P2/6P1/5K2/8 w - - 0 1"
-//            ))
     );
 
     private final int[] pointDistribution = {5, 3, 2, 1};
