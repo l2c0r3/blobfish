155bfeb27222a090f7fe5cb62577e603c9bd9a4b
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-03-08 15:24:30 +0100

    finish evalutil

--- src/test/java/ch/hslu/cas/msed/blobfish/util/EvaluationUtil.java
+++ src/test/java/ch/hslu/cas/msed/blobfish/util/EvaluationUtil.java
@@ -1,29 +1,38 @@
 package ch.hslu.cas.msed.blobfish.util;
 
+import ch.hslu.cas.msed.blobfish.eval.CompositeEvalStrategy;
 import ch.hslu.cas.msed.blobfish.eval.EvalStrategy;
+import ch.hslu.cas.msed.blobfish.eval.RandomEval;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ScanResult;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 // TODO: Review pic
 public class EvaluationUtil {
 
-    public record EvalConfig(EvalStrategy wrapper, String description){}
+    private static final List<Class> EVAL_CLASSES_TO_IGNORE = List.of(
+            CompositeEvalStrategy.class,
+            RandomEval.class
+    );
 
+    public record EvalConfig(EvalStrategy wrapper, String description) {
+    }
+
+    @SuppressWarnings("unchecked")
     public static List<EvalConfig> getAllEvalStrategiesCombinations() {
         try (ScanResult scan = new ClassGraph()
                 .enableClassInfo()
                 .acceptPackages("ch.hslu.cas.msed.blobfish")
                 .scan()) {
 
-            var instances = scan
+            List<EvalStrategy> evalClasses = (List<EvalStrategy>) scan
                     .getClassesImplementing(EvalStrategy.class)
                     .stream()
-                    .filter(e -> !e.getName().contains("Wrapper"))
                     .filter(classInfo -> !classInfo.isInterface())
+                    .filter(classInfo -> !EVAL_CLASSES_TO_IGNORE.contains(classInfo.loadClass()))
                     .map(classInfo -> {
                         try {
                             Class<? extends EvalStrategy> clazz = classInfo.loadClass(EvalStrategy.class);
@@ -32,9 +41,32 @@ public class EvaluationUtil {
                             throw new RuntimeException(e);
                         }
                     })
-                    .toArray();
+                    .toList();
 
-            return List.of();
+            return PowerSetUtil.getPowerSet(evalClasses).stream()
+                    .filter(s -> !s.isEmpty())
+                    .map(EvaluationUtil::mapToEvalConfig)
+                    .toList();
         }
+    }
+
+    private static EvalConfig mapToEvalConfig(List<EvalStrategy> evalStrategies) {
+        if (evalStrategies.isEmpty()) {
+            return null;
+        }
+
+        if (evalStrategies.size() == 1) {
+            var strategy = evalStrategies.getFirst();
+            var name = strategy.getClass().getSimpleName();
+            return new EvalConfig(strategy, name);
+        }
+
+        var strategyCombined = new CompositeEvalStrategy.Builder();
+        evalStrategies.forEach(strategyCombined::add);
+        var strategyDescription = evalStrategies.stream()
+                .map(EvalStrategy::getClass)
+                .map(Class::getSimpleName)
+                .collect(Collectors.joining(" & "));
+        return new EvalConfig(strategyCombined.build(),  strategyDescription);
     }
 }
