56056090f9836d8ed86adcc653dd065f44279cf3
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-03-08 21:39:37 +0100

    wip l

--- src/test/java/ch/hslu/cas/msed/blobfish/util/LatexUtil.java
+++ src/test/java/ch/hslu/cas/msed/blobfish/util/LatexUtil.java
@@ -16,51 +16,60 @@ public class LatexUtil {
         // util class
     }
 
-    public static File generateTableQualityMoves(List<QualityTestResult> results, QualityTest.QualityTestCategory tacticTableToGenerate) {
+    public static File generateTableQualityMoves(List<QualityTestResult> results,
+                                                 QualityTest.QualityTestCategory tacticTableToGenerate) {
         StringBuilder sb = new StringBuilder();
 
         var tacticDescription = escapeLatex(tacticTableToGenerate.getDescription());
+        var caption = escapeLatex("Quality Test von " + tacticTableToGenerate.getDescription());
 
         sb.append("""
                 \\documentclass{article}
                 
+                \\usepackage[a4paper,margin=2cm]{geometry}
                 \\usepackage{array}
                 \\usepackage{multirow}
-                \\usepackage{rotating}
-                \\usepackage{tabularx}
-                
-                \\newcolumntype{Y}{>{\\raggedright\\arraybackslash}X}
+                \\usepackage{longtable}
                 
                 \\begin{document}
-                    \\begin{table}[h]
-                            \\renewcommand{\\arraystretch}{1.2}
-                            
-                            \\begin{tabular}{|
-                                    >{\\raggedright\\arraybackslash}p{0.33\\textwidth}|
-                                    >{\\centering\\arraybackslash}p{0.12\\textwidth}|
-                                    >{\\raggedright\\arraybackslash}p{0.27\\textwidth}|
-                                    >{\\centering\\arraybackslash}p{0.12\\textwidth}|
-                                    >{\\centering\\arraybackslash}p{0.08\\textwidth}|}
-                                \\hline
-                                \\multicolumn{5}{|c|}{\\textbf{%s}} \\\\ \\hline
-                                \\textbf{FEN} & \\textbf{Stockfish move} & \\textbf{Evaluation} & \\textbf{Move} & \\textbf{Pkt} \\\\ \\hline
-                """.formatted(tacticDescription));
+                \\renewcommand{\\arraystretch}{1.2}
+                \\setlength{\\LTleft}{0pt}
+                \\setlength{\\LTright}{0pt}
+                
+                \\begin{longtable}{|
+                        >{\\raggedright\\arraybackslash}p{0.33\\textwidth}|
+                        >{\\centering\\arraybackslash}p{0.12\\textwidth}|
+                        >{\\raggedright\\arraybackslash}p{0.27\\textwidth}|
+                        >{\\centering\\arraybackslash}p{0.12\\textwidth}|
+                        >{\\centering\\arraybackslash}p{0.08\\textwidth}|}
+                \\caption{%s} \\\\
+                \\hline
+                \\multicolumn{5}{|c|}{\\textbf{%s}} \\\\ \\hline
+                \\textbf{FEN} & \\textbf{Stockfish move} & \\textbf{Evaluation} & \\textbf{Move} & \\textbf{Pkt} \\\\ \\hline
+                \\endfirsthead
+                
+                \\hline
+                \\multicolumn{5}{|c|}{\\textbf{%s} -- Fortsetzung} \\\\ \\hline
+                \\textbf{FEN} & \\textbf{Stockfish move} & \\textbf{Evaluation} & \\textbf{Move} & \\textbf{Pkt} \\\\ \\hline
+                \\endhead
+                
+                \\hline
+                \\multicolumn{5}{|r|}{Fortsetzung auf der nächsten Seite} \\\\
+                \\hline
+                \\endfoot
+                
+                \\hline
+                \\endlastfoot
+                """.formatted(caption, tacticDescription, tacticDescription));
 
         results.stream()
                 .filter(testResult -> tacticTableToGenerate.equals(testResult.qualityTestCategory()))
                 .forEach(result -> sb.append(generateTableRow(result)));
 
-        var caption = "Quality Test von " + tacticDescription;
-        sb.append(
-                """
-                        \\hline
-                        \\end{tabular}
-                        \\centering
-                        \\caption{%s}
-                    \\end{table}
+        sb.append("""
+                \\end{longtable}
                 \\end{document}
-                """.formatted(caption));
-
+                """);
 
         var tmpFile = FileUtil.createTmpFile("qualityMoves", "tex");
         try (FileWriter fw = new FileWriter(tmpFile)) {
@@ -76,7 +85,7 @@ public class LatexUtil {
 
         int rowCount = testResult.evalQualityResult().size();
 
-        var fenCutted = String.join("\\\\", testResult.fen().split("(?<=\\G.........)"));
+        var fenCutted = String.join("\\\\", splitFenForLatex(testResult.fen(), 9));
         sb.append("""
                 \\multirow{%d}{*}{
                     \\begin{tabular}{l}
@@ -86,7 +95,7 @@ public class LatexUtil {
                 """.formatted(rowCount, fenCutted));
 
         var stockFishMoves = testResult.stockfishMove().stream()
-                .map(s -> "- " + s)
+                .map(s -> "- " + escapeLatex(s))
                 .collect(Collectors.joining("\\\\"));
         sb.append("""
                 &
@@ -100,6 +109,7 @@ public class LatexUtil {
         var sortedEvalQualityResult = testResult.evalQualityResult().stream()
                 .sorted(Comparator.comparing((QualityTest.EvalQualityResult r) -> r.strategy().description()))
                 .toList();
+
         for (int i = 0; i < rowCount; i++) {
             var result = sortedEvalQualityResult.get(i);
             if (i != 0) {
@@ -107,9 +117,11 @@ public class LatexUtil {
             }
 
             var strategyDescription = escapeLatex(result.strategy().description());
+            var move = escapeLatex(result.move());
+
             sb.append("""
                     & %s & %s & %s \\\\
-                    """.formatted(strategyDescription, result.move(), result.pointWon()));
+                    """.formatted(strategyDescription, move, result.pointWon()));
 
             if (i != rowCount - 1) {
                 sb.append("\\cline{3-5}\n");
@@ -119,6 +131,21 @@ public class LatexUtil {
         }
 
         return sb.toString();
+    }
+
+    private static List<String> splitFenForLatex(String fen, int chunkSize) {
+        String clean = escapeLatex(fen);
+        return clean.lines()
+                .flatMap(line -> chunkString(line, chunkSize).stream())
+                .toList();
+    }
+
+    private static List<String> chunkString(String input, int chunkSize) {
+        var result = new java.util.ArrayList<String>();
+        for (int i = 0; i < input.length(); i += chunkSize) {
+            result.add(input.substring(i, Math.min(input.length(), i + chunkSize)));
+        }
+        return result;
     }
 
     private static String escapeLatex(String s) {
@@ -132,5 +159,4 @@ public class LatexUtil {
                 .replace("{", "\\{")
                 .replace("}", "\\}");
     }
-
 }
