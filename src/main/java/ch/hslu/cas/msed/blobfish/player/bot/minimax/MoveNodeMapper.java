2aef4273e54502c47f1378700382876df4288372
c0r3 <62983504+l2c0r3@users.noreply.github.com>
2026-03-13 12:06:01 +0100

    add PathEvaluation

--- src/main/java/ch/hslu/cas/msed/blobfish/player/bot/minimax/MoveNodeMapper.java
+++ src/main/java/ch/hslu/cas/msed/blobfish/player/bot/minimax/MoveNodeMapper.java
@@ -1,6 +1,9 @@
 package ch.hslu.cas.msed.blobfish.player.bot.minimax;
 
 import ch.hslu.cas.msed.blobfish.player.bot.FirstMoveEvaluation;
+import ch.hslu.cas.msed.blobfish.player.bot.PathEvaluation;
+
+import java.util.ArrayList;
 
 public class MoveNodeMapper {
 
@@ -13,6 +16,24 @@ public class MoveNodeMapper {
                 node = node.parent();
             }
             return new FirstMoveEvaluation(node.move(), moveNode.eval());
+        }
+    }
+
+    public PathEvaluation mapToPathEvaluation(MoveNode moveNode) {
+        if (moveNode == null || moveNode.history() == null) {
+            return null;
+        } else {
+
+            var path = new ArrayList<String>();
+
+            var historyNode = moveNode.history();
+
+            do {
+                path.add(historyNode.move());
+                historyNode = historyNode.parent();
+            } while (historyNode != null);
+
+            return new PathEvaluation(path.reversed(), moveNode.eval());
         }
     }
 }
