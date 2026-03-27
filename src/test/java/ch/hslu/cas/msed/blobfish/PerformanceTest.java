c062deb98841faf512e604b10b3f44f5d9df6a7e
Huzanic Dario <dario.huza@gmail.com>
2026-03-27 23:15:49 +0100

    update PerformanceTest to reflect documentation

--- src/test/java/ch/hslu/cas/msed/blobfish/PerformanceTest.java
+++ src/test/java/ch/hslu/cas/msed/blobfish/PerformanceTest.java
@@ -79,11 +79,8 @@ public class PerformanceTest {
     }
 
     private static final List<PossibleStrategy> possibleStrategies = List.of(
-            new PossibleStrategy(CompositeEvaluationStrategy.builder().add(new MaterialEvaluation()).build(), "Simple material evaluation"),
             new PossibleStrategy(CompositeEvaluationStrategy.builder().add(new MateAwareEvaluation()).add(new MaterialEvaluation()).build(), "Mate aware material evaluation"),
-            new PossibleStrategy(CompositeEvaluationStrategy.builder().add(new PieceSquareEvaluation()).build(), "Simple piece square evaluation"),
             new PossibleStrategy(CompositeEvaluationStrategy.builder().add(new MateAwareEvaluation()).add(new PieceSquareEvaluation()).build(), "Mate aware piece square evaluation"),
-            new PossibleStrategy(CompositeEvaluationStrategy.builder().add(new MaterialEvaluation()).add(new PieceSquareEvaluation()).build(), "Piece square material evaluation"),
             new PossibleStrategy(CompositeEvaluationStrategy.builder().add(new MateAwareEvaluation()).add(new MaterialEvaluation()).add(new PieceSquareEvaluation()).build(), "Mate aware piece square material evaluation")
     );
 
@@ -96,10 +93,10 @@ public class PerformanceTest {
     private record ExecutionConfig(int depth) {
     }
 
-    private static final int DEFAULT_CALCULATION_DEPTH = 4;
+    private static final int DEFAULT_CALCULATION_DEPTH = 5;
     private static final Map<ExecutionConfigKey, ExecutionConfig> executionConfig = ImmutableMap.of(
-            new ExecutionConfigKey(MiniMaxAlphaBetaSequential.class, Collections.emptyList()), new ExecutionConfig(6),
-            new ExecutionConfigKey(MiniMaxAlphaBetaSequentialWithCache.class, Collections.emptyList()), new ExecutionConfig(6)
+            new ExecutionConfigKey(MiniMaxAlphaBetaSequential.class, Collections.emptyList()), new ExecutionConfig(7),
+            new ExecutionConfigKey(MiniMaxAlphaBetaSequentialWithCache.class, Collections.emptyList()), new ExecutionConfig(7)
     );
 
     private static Stream<Arguments> positionProvider() {
